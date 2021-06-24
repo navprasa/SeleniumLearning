8e4e2a07c29eee7013b9f230abde2709bcb76381
Naveen Prasad <navprasa@amazon.com>
2021-06-24 20:51:00 +0530

    Fetching result and comparing both

--- src/main/java/SeleniumRunner.java
+++ src/main/java/SeleniumRunner.java
@@ -1,9 +1,13 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SeleniumRunner {
 
@@ -13,11 +17,24 @@ public class SeleniumRunner {
         driver.manage().window().maximize();
         driver.get("https://www.github.com");
 
+        String searchPhrase = "selenium";
         WebElement Search = driver.findElement(By.cssSelector("[name='q']"));
-        Search.sendKeys("Selenium");
+        Search.sendKeys(searchPhrase);
         Search.sendKeys(Keys.ENTER);
-        driver.quit();
 
 
+        List<String> actualItems = driver.findElements(By.xpath("//ul[@class='repo-list']")).stream()
+                .map(element->element.getText().toLowerCase())
+                .collect(Collectors.toList());
+
+        //Assert.assertTrue(actualItems.stream().allMatch(items->items.matches(searchPhrase)));
+
+        List<String> expectedItems = actualItems.stream()
+                .filter(item->item.contains(searchPhrase))
+                .collect(Collectors.toList());
+        System.out.println(expectedItems);
+        Assert.assertEquals(expectedItems,actualItems);
+
+        driver.close();
     }
 }
